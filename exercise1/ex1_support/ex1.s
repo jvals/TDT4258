0642abb22486ec960fc52b7c9bfc8bbf747987c5
Bjarte Sjursen <b.sjursen@gmail.com>
2015-09-19 20:46:37 +0200

    clean code in ex1.s, vectors.sand efm32gg.s

--- exercise1/ex1_support/ex1.s
+++ exercise1/ex1_support/ex1.s
@@ -1,21 +1,23 @@
-        .syntax unified
-	
-	      .include "efm32gg.s"
-		.include "vectors.s"
+    .syntax unified
 
+	.include "efm32gg.s"
+	.include "vectors.s"
 
 	.section .text
+
  
 	/////////////////////////////////////////////////////////////////////////////
 	//
 	// Reset handler
-  // The CPU will start executing here after a reset
+    // The CPU will start executing here after a reset
 	//
 	/////////////////////////////////////////////////////////////////////////////
+	
 
-	      .globl  _reset
-	      .type   _reset, %function
-        .thumb_func
+
+.globl  _reset
+.type   _reset, %function
+.thumb_func
 _reset:
  	// Enable GPIO clock in cmu
 	ldr R0, cmu_base_addr
@@ -33,10 +35,6 @@ _reset:
 	// Set pins 8-15 to output by writing 0x55555555
 	ldr R1, =0x55555555
 	STR R1, [R0,#GPIO_MODEH]
-
-	// Test LEDs
-	ldr R1, =0b0000000011111111 
-	str R1, [R0,#GPIO_DOUT]
 
 	// Set pins 0-7 to input by writing 0x33333333
 	ldr R0, =GPIO_PC_BASE
@@ -62,21 +60,38 @@ _reset:
 	ldr R1, =ISER0
 	str R0, [R1]
 
+	
 	// Turn off SRAM
 	ldr R0, =0x7
 	ldr R1, =EMU_BASE
 	str R0, [R1, #0x004] // 0x004 is emu_memctrl
 
+	
 	// Disable LFACLK and LFBCLK
 	ldr R0, =0x1
 	lsl R0, #0x10
 	ldr R1, =CMU_BASE
 	str R0, [R1, #0x028] // 0x028 is CMU_LFCLKSEL
 	
-
+	
 	b main
 
-/*	
+	
+	
+cmu_base_addr:
+			.long CMU_BASE
+
+
+
+main:
+	// Set SleepOnExit and SleepDeep
+	ldr R0, =0b110
+	ldr R1, =SCR
+	str R0, [R1]
+	WFI
+
+	
+/*
 start:	//read status of pins 0-7 from GPIO_PC_DIN
 	ldr R0, =GPIO_PA_BASE
 	ldr R4, =GPIO_PC_BASE
@@ -86,27 +101,15 @@ start:	//read status of pins 0-7 from GPIO_PC_DIN
 	
 	b start
 */
-	
-	
-cmu_base_addr:
-			.long CMU_BASE
 
 
-	
-main:
-	// Set SleepOnExit and SleepDeep
-	ldr R0, =0b110
-	ldr R1, =SCR
-	str R0, [R1]
-	WFI
-	/////////////////////////////////////////////////////////////////////////////
-	//
-  // GPIO handler
-  // The CPU will jump here when there is a GPIO interrupt
-	//
-	/////////////////////////////////////////////////////////////////////////////
-
-        .thumb_func
+/////////////////////////////////////////////////////////////////////////////
+//
+// GPIO handler
+// The CPU will jump here when there is a GPIO interrupt
+//
+/////////////////////////////////////////////////////////////////////////////
+.thumb_func
 gpio_handler:
 	// Clear interrupt
 	ldr R0, =GPIO_BASE
@@ -120,11 +123,9 @@ gpio_handler:
 	lsl R5, R5, #8
 	STR R5, [R0, #GPIO_DOUT]
 	bx lr
-	      
-	
-	/////////////////////////////////////////////////////////////////////////////
-	
-        .thumb_func
+
+    
+.thumb_func
 dummy_handler:  
         b .  // do nothing
 
